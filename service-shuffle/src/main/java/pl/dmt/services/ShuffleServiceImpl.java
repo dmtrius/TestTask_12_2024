af8e1820135076bb922e3b382dc90316c7e28d85
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2024-12-21 20:17:59 +0100

    updated Rest/Kafka services

--- service-shuffle/src/main/java/pl/dmt/services/ShuffleServiceImpl.java
+++ service-shuffle/src/main/java/pl/dmt/services/ShuffleServiceImpl.java
@@ -1,22 +1,19 @@
 package pl.dmt.services;
 
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 import pl.dmt.dao.ShuffledData;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 
-@Profile("kafka")
-@Service("shuffleKafkaService")
-public class ShuffleKafkaServiceImpl implements ShuffleService {
+@Service("shuffleService")
+public class ShuffleServiceImpl implements ShuffleService {
 
     private final ShuffledData data;
     private final LogService logService;
 
-    public ShuffleKafkaServiceImpl(@Qualifier("logKafkaService") LogService logService,
-                                   ShuffledData data) {
+    public ShuffleServiceImpl(LogService logService,
+                              ShuffledData data) {
         this.logService = logService;
         this.data = data;
     }
