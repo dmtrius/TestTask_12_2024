41bf950798779b8ef8c50f762ca48bed06f4282e
Dmytro Gordiienko <dmitry.gordienko@gmail.com>
2024-12-21 17:18:48 +0100

    added exception handler

--- service-log/src/main/java/pl/dmt/controllers/GlobalExceptionHandler.java
+++ service-log/src/main/java/pl/dmt/controllers/GlobalExceptionHandler.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.PRECONDITION_FAILED)
+                .body(ex.getMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleApplicationException(Exception ex) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
